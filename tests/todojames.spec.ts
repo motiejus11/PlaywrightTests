428b08299f936e52d52c75ae8a9a7ba00943a781
motiejus1 <motiejus.baliukonis@vka.lt>
2025-03-12 10:04:07 +0200

    first commit

---
+++ tests/todojames.spec.ts
@@ -0,0 +1,66 @@
+import { test, expect } from '@playwright/test';
+
+test.describe('Todojames Tests', () => {
+    test.beforeEach(async ({page}) => {
+        await page.goto('https://todolist.james.am/#/')
+    });
+
+    test('has title h1', async ({ page }) => {
+        //kad reikia pasirinkti pagal id, pagal class'e, pagal elementa, arba pagal elementa ir jame esanti teksta
+        // cy.get('.klase')
+        //cy.get('#id').contains('tekstas')
+
+        // h1 tago ir kas jame parasyta pagal Regex
+        await expect(page).toHaveTitle("To Do List");
+
+        //pasirinkom su locator h1 ir patikrinom ar yra tekstas ToDoList
+        await expect(page.locator('h1')).toHaveText('To Do List');
+
+        //h1 taga, kurio tekstas yra To Do List cy.get('h1').contains('To Do List')
+
+        //taip galima tik tam tikra role atliekancius elementus, mygtuka
+        //h1,h2,h3 iki h6, kurie turi teksta To Do lsit
+        await expect(page.getByRole('heading', {name: 'To Do List'})).toBeVisible();
+
+        //pagal turima teksta
+
+        await expect(page.getByText('To Do List')).toBeVisible();
+      });
+    
+    test('add new item', async ({ page }) => {
+        // it('Create new to do', () => {
+        //     cy.get('input.new-todo').type('1 uzduotis{enter}');
+        //     cy.contains('ul.todo-list li', '1 uzduotis').should('be.visible')
+        // });
+
+        await page.locator('input.new-todo').fill('1 uzduotis');
+        await page.keyboard.press('Enter');
+        await page.locator('input.new-todo').fill('1 uzduotis');
+        await page.keyboard.press('Enter');
+        await page.locator('input.new-todo').fill('1 uzduotis');
+        await page.keyboard.press('Enter');
+
+        // await page.getByText('1 uzduotis').click();      
+
+
+        // console.log(page.getByText('1 uzduotis'));
+        // 
+        await expect(page.getByText('1 uzduotis').nth(1)).toBeVisible();
+
+
+
+    });
+    
+    test('delete item', async ({ page }) => {
+    
+    });
+    
+    test('update item', async ({ page }) => {
+    
+    });
+    
+    test('count items', async ({ page }) => {
+    
+    });
+
+});
